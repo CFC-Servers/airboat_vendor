fea4b01813ceecd7fdce2fae04db0627ca46957a
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-08-02 14:21:56 -0400

    Kappa

--- lua/airboat_vendor/sh_config.lua
+++ lua/airboat_vendor/sh_config.lua
@@ -12,3 +12,9 @@ AirboatVendor.Config.spawnPoints["rp_southside_day"] = {
     { pos = Vector( 4649, -5859, -381 ), ang = Angle( 0, 90, 0 ) },
     { pos = Vector( 4601, -6265, -389 ), ang = Angle( 0, 90, 0 ) }
 }
+
+AirboatVendor.Config.spawnPoints["rp_downtown_evilmelon_v1"] = {
+    { pos = Vector( 742, -422, -191 ), ang = Angle( 0, 0, 0 ) },
+    { pos = Vector( 652, -422, -191 ), ang = Angle( 0, 0, 0 ) },
+    { pos = Vector( 553, -422, -191 ), ang = Angle( 0, 0, 0 ) }
+}
